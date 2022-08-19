8cb51e53d22ffdad2873638698018633a5452769
frkylmz <farukyilmaz420@gmail.com>
2022-08-19 16:19:46 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Contacts from "./components/Contacts";
+import Edit from "./components/Contacts/Edit";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <div id="container">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Contacts}></Route>
+            <Route path="/edit/:id" component={Edit}></Route>
+          </Switch>
+        </Router>
+      </div>
+    </div>
+  );
+}
+
+export default App;
